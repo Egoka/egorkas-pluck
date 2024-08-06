f69604c6c470d4eff1a35414b4cd40f5175c5299
Egoka <eg.bonda2014@gmail.com>
2024-08-07 00:00:59 +0300

    test(log): update text title log

--- lib/index.test.ts
+++ lib/index.test.ts
@@ -1,7 +1,7 @@
 import { describe, it, expect, vi } from "vitest"
 import { log } from "fishtvue/index"
 describe("Testing lib", () => {
-  it("log", () => {
+  it("log update title", () => {
     const logSpy = vi.spyOn(console, "log")
     log()
     expect(logSpy).toHaveBeenCalledWith("Work project hello")
