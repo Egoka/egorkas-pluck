8d5ac2e58dd1898372a5a57af553d1d572493d70
Egoka <eg.bonda2014@gmail.com>
2024-08-06 23:19:54 +0300

    ci(up): update ci

--- release.config.cjs
+++ release.config.cjs
@@ -84,7 +84,7 @@ const config = {
     [
       "@semantic-release/git",
       {
-        assets: ["dist/*.js", "dist/*.js.map"],
+        assets: ["CHANGELOG.md", "package.json"],
         message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
       }
     ],
