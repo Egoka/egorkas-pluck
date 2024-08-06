bb0effb8b413e20e5355ed92fe3c83f6ab1c7812
Egoka <eg.bonda2014@gmail.com>
2024-08-06 12:25:07 +0300

    fix(log)

--- lib/index.test.ts
+++ lib/index.test.ts
@@ -4,7 +4,7 @@ describe("Testing lib", () => {
   it("log", () => {
     const logSpy = vi.spyOn(console, "log")
     log()
-    expect(logSpy).toHaveBeenCalledWith("Work project")
+    expect(logSpy).toHaveBeenCalledWith("Work project!")
     logSpy.mockRestore()
   })
 })
